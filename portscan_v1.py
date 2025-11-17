8a49e4ea9625a19ee98709f23adbb7215978a31b
Obama <obama_nation@kali.host>
2025-11-17 10:32:23 -0300

    feat: Varredura em multiplas portas utilizando threading

--- portscan_v1.py
+++ portscan_v1.py
@@ -17,30 +17,60 @@ def main():
     description="Scanner de Portas Simples",
     epilog="Exemplo: python script.py -H 192.168.0.1 -P 80"
 )
-    parser.add_argument("-H","--host",help="Host a ser escaneada")
-    parser.add_argument("-P", "--port",type=int, help="Porta a ser escaneada")
-
+    parser.add_argument("-H","--host",help="Host a ser escaneado")
+    parser.add_argument("-P", "--port", help="Porta a ser escaneada.Multiplas portas: <PORT>,<PORT>,<PORT>,<PORT>")
     args = parser.parse_args()
+
+
+
     if args.host is None or args.port is None:
         print(parser.print_help())
         sys.exit(1)
-    try:    
-        scan_port(args.host, args.port)
+    
+    # elif not ipaddress.ip_address(args.host):
+    #     try:
+    #         tgt_ip = resolve_dns(args.host)
+    
+    #     except:
+    #         print("[*] Invalid host!")
+    
+    # else:
+    #     try:
+    #         tgt_name = resolve_reverse_dns(args.host)
+    #     except:
+    #         continue
+    try:
         
+        threads = []
+        port_list = [int(p) for p in args.port.split(",")]
+
+        print(f"Scan no host:{args.host}")
+        start_time = perf_counter()
+        for port in port_list:
+            thread = threading.Thread(target=scan_port, args=(args.host, port))
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+        
+        end_time = perf_counter()
+        print(f"Scan finalizado em: {(end_time - start_time):.4f}s")
+
     except Exception as error:
         print(f"[{vermelho}*{reset}]Erro ocorrido:{error}")
 
 
 def scan_port(host:str,port:int):
-    start_time = perf_counter()
-
+    # start_time = perf_counter()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("teste")
     if sock.connect_ex((host,port)) == 0:
         print(f"[{verde}*{reset}]Port {port} is open")
-        end_time = perf_counter()
-        print(f"[{verde}*{reset}]Scanning time:{(start_time - end_time):.4f}")
+                  
     sock.close()
+    # end_time = perf_counter()
+    # print(f"[{verde}*{reset}]Scanning time:{(start_time - end_time):.4f}")
+
     
 if __name__ == "__main__":
     main()
