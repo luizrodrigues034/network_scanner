e8cf0ffdfc327bc82b67d1ffcc07ce1b93a344fe
Luiz Rodrigues <luizrodriguescode@gmail.com>
2025-11-17 11:12:44 -0300

    feat: Funcionalidade de reverse dns

--- portscan_v1.py
+++ portscan_v1.py
@@ -6,10 +6,12 @@ import threading
 import ipaddress
 import sys
 from time import perf_counter 
+import regex as re
 
 vermelho = "\033[31m"
 verde = "\033[32m"
 reset = "\033[0m"
+regex_ipv4 = r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 
 def main():
     
@@ -19,6 +21,8 @@ def main():
 )
     parser.add_argument("-H","--host",help="Host a ser escaneado")
     parser.add_argument("-P", "--port", help="Porta a ser escaneada.Multiplas portas: <PORT>,<PORT>,<PORT>,<PORT>")
+    parser.add_argument("-R", "--reverse-resolver", action="store_true", help="Tenta descobrir o nome do host (DNS Reverso)")
+    
     args = parser.parse_args()
 
 
@@ -26,26 +30,22 @@ def main():
     if args.host is None or args.port is None:
         print(parser.print_help())
         sys.exit(1)
-    
-    # elif not ipaddress.ip_address(args.host):
-    #     try:
-    #         tgt_ip = resolve_dns(args.host)
-    
-    #     except:
-    #         print("[*] Invalid host!")
-    
-    # else:
-    #     try:
-    #         tgt_name = resolve_reverse_dns(args.host)
-    #     except:
-    #         continue
+
     try:
-        
         threads = []
         port_list = [int(p) for p in args.port.split(",")]
+        if re.match(regex_ipv4, args.host) and args.reverse_resolver:
+            print(f"Scan no host:{args.host}")
+            print(f"Name:{reverse_dns(args.host)}")
 
-        print(f"Scan no host:{args.host}")
+        elif(not re.match(regex_ipv4, args.host)): 
+            print(f"Domain:{args.host}")
+            print(f"Target Ip:{dns_resolver(args.host)}")
+        else:
+            print(f"Scan no host:{args.host}")
+        
         start_time = perf_counter()
+        
         for port in port_list:
             thread = threading.Thread(target=scan_port, args=(args.host, port))
             thread.start()
@@ -62,15 +62,31 @@ def main():
 
 
 def scan_port(host:str,port:int):
-    # start_time = perf_counter()
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
     if sock.connect_ex((host,port)) == 0:
         print(f"[{verde}*{reset}]Port {port} is open")
                   
     sock.close()
-    # end_time = perf_counter()
-    # print(f"[{verde}*{reset}]Scanning time:{(start_time - end_time):.4f}")
 
+
+def reverse_dns(tgt_ip)-> str:
+    try:
+        tgt_name = socket.gethostbyaddr(tgt_ip)
+        return tgt_name[0]
+    except: 
+        return "Não foi possivel resolver o dns"
+
+
+def dns_resolver(tgt_name)->str or None:
+    try:
+        tgt_ip = socket.gethostbyname(tgt_name)
+    
+        return tgt_ip
+    
+    except: 
+        return "Não foi possivel resolver o dns"
     
 if __name__ == "__main__":
     main()
